2a59ec9f4ef7c08064ddeeea97d37c3286d82d14
edr <github@kendar.org>
2018-07-26 17:07:03 +0200

    Changed to use already present poms

--- Integration/MavenPush.bat
+++ Integration/MavenPush.bat
@@ -1,5 +1,5 @@
 @echo off
-
+REM maven_exes\lib\slf4j-api-1.7.25.pom
 SET APIKEY=E7446C12-BD80-4272-3332-09914BE6EBC8
 SET NUROOT=http://localhost:9088/maven
 
@@ -9,7 +9,7 @@ cd bin
 
 echo Maven.3.5.4, Upload of simple file
 
-mvn deploy:deploy-file -DgroupId=log4j -DartifactId=project -Dversion=1.2.17 -DgeneratePom=true -Dpackaging=jar -DrepositoryId=nexus -Durl=%NUROOT% -DpomFile=%CUDIR%\log4j-1.2.17.pom -Dfile=%CUDIR%\log4j-1.2.17.jar
+mvn deploy:deploy-file -DgroupId=org -DartifactId=slf4j -Dversion=1.7.25 -DgeneratePom=true -Dpackaging=jar -DrepositoryId=local -Durl=%NUROOT% -DpomFile=%CUDIR%\lib\slf4j-api-1.7.25.pom -Dfile=%CUDIR%\lib\slf4j-api-1.7.25.jar
 
 cd ..
 
