43a676acc475699b6f8914f4e9be623c1ba9a174
Enrico Da Ros <github@kendar.org>
2018-07-30 17:31:22 +0200

    Updated docs

--- Maven.Lib/MavenApiIntializer.cs
+++ Maven.Lib/MavenApiIntializer.cs
@@ -26,24 +26,65 @@ namespace Maven
         }
         public void Initialize(IRepositoryServiceProvider repositoryServiceProvider)
         {
+            var avail = _repositoryEntitiesRepository.GetAll().FirstOrDefault(a => a.Prefix == "repo.maven.apache.org");
+            //https://repo.maven.apache.org/maven2/
+            if (avail == null)
+            {
+                //var data = _assemblyUtils.ReadRes<MavenApiIntializer>("maven.org.settings.json");
+                avail = new RepositoryEntity
+                {
+                    Mirror = true,
+                    Prefix = "maven.apache",
+                    Type = "maven",
+                    Address = "/maven.apache",
+                    PackagesPath = "maven.apache",
+                    Settings = string.Empty
+                };
+                _repositoryEntitiesRepository.Save(avail);
+            };
 
-            repositoryServiceProvider.RegisterApi(new Maven2_Push_Package(_repositoryEntitiesRepository,
-              "*GET", "*PUT", @"/{repo}/{*path}/" +
-                @"{pack#^(?<package>[0-9A-Za-z\-\.]+)$}/" +
-                @"{ver#^(?<major>\d+)" +
-                @"(\.(?<minor>\d+))?" +
-                @"(\.(?<patch>\d+))?" +
-                @"(\.(?<extra>\d+))?" +
-                @"(\-(?<pre>[0-9A-Za-z\.]+))?" +
-                @"(\-(?<build>[0-9A-Za-z\-\.]+))?$}/" +
-                @"{filename#^(?<fullpackage>(?:(?!\b(?:jar|pom)\b)[0-9A-Za-z\-\.])+)?" +
-                @"\.(?<type>(jar|pom))" +
-                @"(\.(?<subtype>(asc|md5|sha1)))?$}"));
+            var local = _repositoryEntitiesRepository.GetAll().FirstOrDefault(a => a.Prefix == "maven.local");
 
-            repositoryServiceProvider.RegisterApi(new Maven2_Push(_repositoryEntitiesRepository,
-              "*GET", "*PUT", @"/{repo}/{*path}/" +
-                @"{pack#^(?<package>[0-9A-Za-z\-\.]+)$}/" +
-                @"{meta#^(?<filename>(maven-metadata.xml))(\.(?<subtype>(asc|md5|sha1)))?$}"));
+            if (local == null)
+            {
+                //var data = _assemblyUtils.ReadRes<MavenApiIntializer>("nuget.org.settings.json");
+                local = new RepositoryEntity
+                {
+                    Mirror = false,
+                    Prefix = "maven.local",
+                    Type = "maven",
+                    Address = "/maven.local",
+                    PackagesPath = "maven.local",
+                    Settings = string.Empty
+                };
+                _repositoryEntitiesRepository.Save(local);
+            };
+
+            foreach (var item in _repositoryEntitiesRepository.GetByType("nuget"))
+            {
+
+                repositoryServiceProvider.RegisterApi(new Maven2_Push_Package(_repositoryEntitiesRepository,
+                    "*GET", "*PUT",
+                    @"/{repo}/{*path}/" + ///maven.local/org/slf4j
+                    @"{pack#^(?<package>[0-9A-Za-z\-\.]+)$}/" + //slf4j-api/
+                    @"{ver#^(?<major>\d+)" + //1.7.25/
+                    @"(\.(?<minor>\d+))?" +
+                    @"(\.(?<patch>\d+))?" +
+                    @"(\.(?<extra>\d+))?" +
+                    @"(\-(?<pre>[0-9A-Za-z\.]+))?" +
+                    @"(\-(?<build>[0-9A-Za-z\-\.]+))?$}/" +
+                    @"{filename#^(?<fullpackage>(?:(?!\b(?:jar|pom)\b)[0-9A-Za-z\-\.])+)?" +
+                    @"\.(?<type>(jar|pom))" +
+                    @"(\.(?<subtype>(asc|md5|sha1)))?$}".
+                        Replace("{repo}", item.Prefix))); //slf4j-api-1.7.25.jar.md5
+
+                repositoryServiceProvider.RegisterApi(new Maven2_Push(_repositoryEntitiesRepository,
+                    "*GET", "*PUT",
+                    @"/{repo}/{*path}/" +//maven.local/org/slf4j/
+                    @"{pack#^(?<package>[0-9A-Za-z\-\.]+)$}/" + //slf4j-api/
+                    @"{meta#^(?<filename>(maven-metadata.xml))(\.(?<subtype>(asc|md5|sha1)))?$}".
+                        Replace("{repo}", item.Prefix))); //maven-metadata.xml.asc
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 # RepositoryCache
 Nuget, Maven, NPM local cache server
 
-* all apis start with /type/prefix/api where type is n=nuget m=maven n=npm
 * it is possible to push on remote repos
 * tree view only for maven
 * maven and npm search similar to nuget
+
+## Common API
+
+* _GET /v1/index.json_ Retrieve all the existing databases
+
+## Maven API
+
+* _PUT /{repo}/{path}/{packageId}/{version}/{packageId}.{version}.\[jar|pom\](.\[md5|asc|sha1\]))_	To upload package, pom and the signatures
+* _PUT /{repo}/{path}/{packageId}/maven-metadata.xml(.\[md5|asc|sha1\]))_	To upload the maven metadata for the uploaded package
+
+## Nuget API
+
+* _GET /{repo}/v3/index.json_	List of avaliable APIs
+* _POST /{repo}/v2/publish_	Publish package
+* _DELETE /{repo}/v2/publish_	Delist package
+* _PUT /{repo}/v2/publish_	Relist package
+* _GET /{repo}/v3/registration/{semver}/{packageid}/index.json_ Registration pages for the specific package and semver
+* _GET /{repo}/v3/registration/{semver}/{packageid}/page/{from}/{to}.json_ List of registrations paged by version
+* _GET /{repo}/v3/registration/{semver}/{packageid}/{version}.json_ Registration for the specific package version and semver
+* _GET /{repo}/v3/query_ Search the packages
+* _GET /{repo}/v3/container/{idLower}/{versionLower}/{fullversion}.nupkg_ Download the package
+* _GET /{repo}/v3/container/{packageid}/index.json_ List the versions for the package
+* _GET /{repo}/v3/catalog/data/{date}/{fullPackage}.json_ Retrieve alla data included dependencies for package
+* _GET /{repo}/custom/load_ Load nupkg from an existing directory
+
+
+
